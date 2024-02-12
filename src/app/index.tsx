8f00e6ffbfe99585e5452aea8d103f4b070c6392
Jhonny515 <jonatascemporcentojesus@gmail.com>
2024-02-12 00:20:39 -0300

    Aula 2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,62 @@
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { View, FlatList } from "react-native";
-import { CATEGORIES } from "@/utils/data/products";
-import { useState } from "react";
+import { Product } from "@/components/product";
+import { View, FlatList, SectionList, Text } from "react-native";
+import { CATEGORIES, MENU } from "@/utils/data/products";
+import { useState, useRef } from "react";
+import { Link } from 'expo-router';
+import { useCartStore } from "@/stores/cart-stores";
 
 export default function Home() {
+    const store = useCartStore();
     const [category, setCategory] = useState(CATEGORIES[0]);
-    
-    function handleCategorySelect(selectedCategory:string) {
+
+    const sectionListRef = useRef<SectionList>(null);
+    const cartQuantityItems = store.products.reduce((total, product) => total + product.quantity, 0);
+
+    function handleCategorySelect(selectedCategory: string) {
         setCategory(selectedCategory);
+
+        const sectionIndex = CATEGORIES.findIndex( category => category === selectedCategory);
+
+        if (sectionListRef.current) {
+            sectionListRef.current.scrollToLocation({
+                animated: true,
+                sectionIndex,
+                itemIndex: 0,
+            });
+        }
     }
-    
+
     return (
         <View className="flex-1 pt-8">
-            <Header title="Faça seu pedido" cartQuantityItems={3} />
+            <Header title="Faça seu pedido" cartQuantityItems={cartQuantityItems} />
 
             <FlatList
                 data={CATEGORIES}
                 keyExtractor={(item) => item}
-                renderItem={({ item }) => <CategoryButton title={item} isSelected={item  === category} onPress={() => handleCategorySelect(item)} />}
+                renderItem={({ item }) => <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)} />}
                 horizontal
                 className="max-h-10 mt-5"
                 contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
-                showsHorizontalScrollIndicator={false} />
+                showsHorizontalScrollIndicator={false}
+            />
+
+            <SectionList
+                ref={sectionListRef}
+                sections={MENU}
+                keyExtractor={(item) => item.id}
+                stickySectionHeadersEnabled={false}
+                renderItem={({ item }) => (
+                    <Link href={`/product/${item.id}`} asChild>
+                        <Product data={item} />
+                    </Link>
+                )}
+                renderSectionHeader={({ section: { title } }) => <Text className="text-xl text-white font-heading mt8 mb-3">{title}</Text>}
+                className="flex-1 p-5"
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
+            />
         </View>
 
     )
---
+++ src/components/link-button.tsx
@@ -0,0 +1,14 @@
+import { Link, LinkProps } from "expo-router";
+
+type LinkButtonProps = LinkProps<String> & {
+    title: string;
+    href: string;
+}
+
+export function LinkButton({ title, ...rest }: LinkButtonProps) {
+    return (
+        <Link className="text-slate-300 text-center text-base font-body" {...rest}>
+            {title}
+        </Link>
+    )
+}
