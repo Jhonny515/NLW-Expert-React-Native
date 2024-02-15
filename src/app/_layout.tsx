c81edb6b46a636e5622a496c8aac8dc84d7ca096
Jhonny515 <jonatascemporcentojesus@gmail.com>
2024-02-15 12:42:53 -0300

    Melhorias no Toast

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,33 +2,15 @@ import Toast from "@/components/Toast";
 import { Loading } from "@/components/loading";
 import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold, useFonts } from "@expo-google-fonts/inter";
 import { Slot } from "expo-router";
-import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Layout() {
-    const [toast, setToast] = useState(0);
     const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_500Medium,
-    Inter_600SemiBold,
-    Inter_700Bold
-});
-
-    let toastTimeout: NodeJS.Timeout;
-
-    Toast({
-        toastConditional: (value) => setToast(value),
-        isShowing: toast,
+        Inter_400Regular,
+        Inter_500Medium,
+        Inter_600SemiBold,
+        Inter_700Bold
     });
-
-    useEffect(() => {
-        if (toast != 0) {
-            clearTimeout(toastTimeout)
-        toastTimeout = setTimeout(
-            () => setToast(0),
-            5000)
-        }
-    }, [toast])
 
     if (!fontsLoaded) {
         return <Loading />;
@@ -36,10 +18,10 @@ export default function Layout() {
 
 
     return (
-        <SafeAreaView className = "flex-1 bg-slate-900" >
+        <SafeAreaView className="flex-1 bg-slate-900" >
             <Slot />
 
-            {toast != 0 && <Toast.render />}
+            <Toast />
         </SafeAreaView >
     )
 }
